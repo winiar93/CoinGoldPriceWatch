3cae7b57970d9114676ce7b527d2a72e0ed9e61a
Mike <winiar93@gmail.com>
2022-07-14 20:13:40 +0200

    added node.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get("/", (req, res) => {
-    process.exit(10);
     client.get('visits', (err, visits) => {
         res.send('Number of visits: ' + visits);
         client.set('visits', parseInt(visits) + 1);
