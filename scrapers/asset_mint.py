c2596cbbcd4e44c9b7ad5b5f00045f57d0465126
Mike <winiar93@gmail.com>
2022-08-13 15:31:53 +0200

    added html table

--- scrapers/asset_mint.py
+++ scrapers/asset_mint.py
@@ -5,22 +5,18 @@ import logging
 import time
 from rejson import Client, Path
 
-rejson_client = Client(host='localhost', port=6379, decode_responses=True)
-
-
-# consider bigger dict of url for each gold coin or scrape whole webside ?
-urls_dict = {"Canadian Maple Leaf": "https://mennicakapitalowa.pl/product-pol-153-moneta-zlota-Kanadyjski-Lisc-Klonu-1oz-2022-najtaniej.html",
-            "Krugerrands": "https://mennicakapitalowa.pl/product-pol-434-moneta-zlota-Krugerrand-1oz-2022-najtaniej.html",
-            "Vienna Philharmonic": "https://mennicakapitalowa.pl/product-pol-148-moneta-zlota-Wiedenski-Filharmonik-1oz-2022-najtaniej.html",
-            "Australian Kangaroo": "https://mennicakapitalowa.pl/product-pol-43-moneta-zlota-Australijski-Kangur-2022-1oz.html"}
+rejson_client = Client(host='rejson', port=6379, decode_responses=True)
 
 logging.basicConfig(level=logging.ERROR)
 
 
 class GoldMarketNo1:
     """https://mennicakapitalowa.pl"""
-    def __init__(self, urls_dict) -> None:
-        self.urls_dict = urls_dict
+    def __init__(self) -> None:
+        self.urls_dict = {"Canadian Maple Leaf": "https://mennicakapitalowa.pl/product-pol-153-moneta-zlota-Kanadyjski-Lisc-Klonu-1oz-2022-najtaniej.html",
+            "Krugerrands": "https://mennicakapitalowa.pl/product-pol-434-moneta-zlota-Krugerrand-1oz-2022-najtaniej.html",
+            "Vienna Philharmonic": "https://mennicakapitalowa.pl/product-pol-148-moneta-zlota-Wiedenski-Filharmonik-1oz-2022-najtaniej.html",
+            "Australian Kangaroo": "https://mennicakapitalowa.pl/product-pol-43-moneta-zlota-Australijski-Kangur-2022-1oz.html"}
         self.output_data = {'Mint':'mennicakapitalowa.pl'}
 
     def get_coin_prices(self):
@@ -35,7 +31,7 @@ class GoldMarketNo1:
                     raw_price = raw_price.replace(" ","").replace(",",".")
                     price = re.findall("\d+\.\d+", raw_price)
                     price = float(price[0])
-                    tmp_data = {'Coin': f'{coin}', 'Price': f'{price}'}
+                    tmp_data = {'Coin': f'{coin}', 'Price': f'{price}', "Link": url}
                     self.output_data.update(tmp_data)
                     rejson_client.jsonset(f'mennicakapitalowa:{cnt}', Path.rootPath(), self.output_data)
                     print(coin, price)
@@ -46,9 +42,42 @@ class GoldMarketNo1:
             else:
                 logging.critical(f"Error while downloading coin price {r.status_code} - {url}", exc_info=True)
 
-gmn1 = GoldMarketNo1(urls_dict)
+
+class GoldMarketNo2:
+    """https://www.mennica.com.pl/"""
+    def __init__(self) -> None:
+        self.urls_dict = {"Canadian Maple Leaf": "https://www.mennica.com.pl/produkty-inwestycyjne/produkt-inwestycyjny/lisc-klonowy-1oz-zlota-moneta-bulionowa-maple-leaf",
+            "Krugerrands":"https://www.mennica.com.pl/produkty-inwestycyjne/produkt-inwestycyjny/krugerrand-1-oz-zlota-moneta-bulionowa",
+            "Vienna Philharmonic": "https://www.mennica.com.pl/produkty-inwestycyjne/produkt-inwestycyjny/filharmonicy-wiedenscy-1oz-zlota-moneta-bulionowa",
+            "Australian Kangaroo": "https://www.mennica.com.pl/produkty-inwestycyjne/produkt-inwestycyjny/australijski-kangur-1oz-zlota-moneta-bulionowa"}
+        self.output_data = {'Mint':'mmennica.com.pl'}
+    def get_coin_prices(self):
+        for cnt, coin in enumerate(self.urls_dict):
+            url = self.urls_dict[coin]
+            r = requests.get(url)
+            if r.status_code == 200:
+                try:
+                    doc = BeautifulSoup(r.text, "html.parser")
+                    raw_price = doc.find_all("span", {"class": "product_price_value_current"})[0]
+                    raw_price = raw_price.text.strip()
+                    raw_price = re.sub(r"\s+", "", raw_price)
+                    raw_price = raw_price.replace(",",".")
+                    price = re.findall("\d+\.\d+", raw_price)
+                    price = float(price[0])
+                    tmp_data = {'Coin': f'{coin}', 'Price': f'{price}', "Link": url}
+                    self.output_data.update(tmp_data)
+                    rejson_client.jsonset(f'www.mennica.com.pl:{cnt}', Path.rootPath(), self.output_data)
+                    print(coin, price)
+
+                except Exception as e:
+                    logging.critical(e, exc_info=True)
+            else:
+                logging.critical(f"Error while downloading coin price {r.status_code} - {url}", exc_info=True)
+
+gmn2 = GoldMarketNo2()
+gmn1 = GoldMarketNo1()
 
 while True:
-    data = gmn1.get_coin_prices()
+    gmn1.get_coin_prices()
+    gmn2.get_coin_prices()
     time.sleep(30)
-# consider list comprehension to store data ?
--- web/flask_app.py
+++ web/flask_app.py
@@ -1,11 +1,10 @@
-from turtle import heading
 from flask import Flask, render_template
 from rejson import Client, Path
 import redis
 import pandas as pd
 
 try:
-    client = Client(host='localhost', port=6379, decode_responses=True)
+    client = Client(host='rejson', port=6379, decode_responses=True)
     print("connected to redis")
 except Exception as e:
     print(f"{e}")
@@ -14,19 +13,30 @@ app = Flask(__name__)
 
 headings = ("Mint", "Coin type", "Price")
 
+def make_clickable(url, name):
+    return '<a href="{}" rel="noopener noreferrer" target="_blank">{}</a>'.format(url,name)
+
 def get_redis_data():
     tmp_data_list = []
     keys_list = client.keys()
     for k in keys_list:
-        tmp_data_list.append(client.json().get(k))
+        tmp_data_list.append(client.jsonget(k, Path.rootPath()))
     
+    df = pd.DataFrame(tmp_data_list)
+    df['Link'] = df['Link'].apply(lambda x: f'<a href="{x}">Go to coin</a>')
+    df['Price'] = df['Price'].astype(float)
+    df.sort_values(by=['Price'], inplace=True)
+
+
+    table = df.to_html(index=False, escape=False)
+    return table
 
 
 
 @app.route('/')
 def hello():
-    
-    return render_template("table.html", headings=headings, data=data)
+    table = get_redis_data()
+    return render_template("table.html", table=table)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
